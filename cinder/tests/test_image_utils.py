ef027a88d99c531cdbbd0975cb6ae3790ad5ba9e
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2015-02-16 17:53:30 +0200

    Fix qemu-img info vhd format issue
    Due to the fact that qemu-img seeks the VHD signature at the wrong
    offset, fixed size VHD images are probed as 'raw'.
    
    This patch is a workaorund for the issue, adding an extra check
    seeking the VHD 'conectix' at the right offset.
    
    This workaround can be removed after the following bug fix merges:
    https://patchwork.ozlabs.org/patch/375762/

--- cinder/tests/test_image_utils.py
+++ cinder/tests/test_image_utils.py
@@ -27,46 +27,36 @@ from cinder import test
 
 
 class TestQemuImgInfo(test.TestCase):
-    @mock.patch('cinder.openstack.common.imageutils.QemuImgInfo')
-    @mock.patch('cinder.utils.execute')
-    def test_qemu_img_info(self, mock_exec, mock_info):
-        mock_out = mock.sentinel.out
-        mock_err = mock.sentinel.err
-        test_path = mock.sentinel.path
-        mock_exec.return_value = (mock_out, mock_err)
-
-        output = image_utils.qemu_img_info(test_path)
-        mock_exec.assert_called_once_with('env', 'LC_ALL=C', 'qemu-img',
-                                          'info', test_path, run_as_root=True)
-        self.assertEqual(mock_info.return_value, output)
-
-    @mock.patch('cinder.openstack.common.imageutils.QemuImgInfo')
-    @mock.patch('cinder.utils.execute')
-    def test_qemu_img_info_not_root(self, mock_exec, mock_info):
-        mock_out = mock.sentinel.out
-        mock_err = mock.sentinel.err
-        test_path = mock.sentinel.path
-        mock_exec.return_value = (mock_out, mock_err)
-
-        output = image_utils.qemu_img_info(test_path, run_as_root=False)
-        mock_exec.assert_called_once_with('env', 'LC_ALL=C', 'qemu-img',
-                                          'info', test_path, run_as_root=False)
-        self.assertEqual(mock_info.return_value, output)
-
     @mock.patch('cinder.image.image_utils.os')
+    @mock.patch.object(image_utils, '_is_vhd')
     @mock.patch('cinder.openstack.common.imageutils.QemuImgInfo')
     @mock.patch('cinder.utils.execute')
-    def test_qemu_img_info_on_nt(self, mock_exec, mock_info, mock_os):
+    def _test_qemu_img_info(self, mock_exec, mock_info, mock_is_vhd, mock_os,
+                            is_vhd=False, os_name='posix', run_as_root=False):
         mock_out = mock.sentinel.out
         mock_err = mock.sentinel.err
         test_path = mock.sentinel.path
         mock_exec.return_value = (mock_out, mock_err)
-        mock_os.name = 'nt'
+        mock_is_vhd.return_value = is_vhd
+        mock_os.name = os_name
 
-        output = image_utils.qemu_img_info(test_path)
-        mock_exec.assert_called_once_with('qemu-img', 'info', test_path,
-                                          run_as_root=True)
+        output = image_utils.qemu_img_info(test_path, run_as_root=run_as_root)
+
+        expected_args = ('qemu-img', 'info', test_path)
+        if os_name != 'nt':
+            expected_args = ('env', 'LC_ALL=C') + expected_args
+        mock_exec.assert_called_once_with(*expected_args,
+                                          run_as_root=run_as_root)
         self.assertEqual(mock_info.return_value, output)
+
+    def test_qemu_img_info_not_root(self):
+        self._test_qemu_img_info()
+
+    def test_qemu_img_info_on_nt(self):
+        self._test_qemu_img_info(os_name='nt')
+
+    def test_qemu_img_info_vhd_image(self):
+        self._test_qemu_img_info(is_vhd=True)
 
     @mock.patch('cinder.utils.execute')
     def test_get_qemu_img_version(self, mock_exec):
