a2558182e7c7db4ca8c8944298bfcf060a1f3bb9
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2016-11-28 13:46:50 +0200

    Move windows drivers to versioned objects
    Cinder has transitioned to using versioned objects,
    the Windows SMB driver should reflect this as well.
    
    Change-Id: I0a68177f350eeb8ab0b0a35cc42235a04c7979db

--- cinder/volume/drivers/windows/smbfs.py
+++ cinder/volume/drivers/windows/smbfs.py
@@ -332,7 +332,7 @@ class WindowsSmbfsDriver(remotefs_drv.RemoteFSPoolMixin,
     def _do_create_volume(self, volume):
         volume_path = self.local_path(volume)
         volume_format = self.get_volume_format(volume)
-        volume_size_bytes = volume['size'] * units.Gi
+        volume_size_bytes = volume.size * units.Gi
 
         if os.path.exists(volume_path):
             err_msg = _('File already exists at: %s') % volume_path
@@ -428,7 +428,7 @@ class WindowsSmbfsDriver(remotefs_drv.RemoteFSPoolMixin,
             return
 
         backing_file_full_path = os.path.join(
-            self._local_volume_dir(snapshot['volume']),
+            self._local_volume_dir(snapshot.volume),
             backing_file)
         self._vhdutils.create_differencing_vhd(new_snap_path,
                                                backing_file_full_path)
@@ -523,7 +523,7 @@ class WindowsSmbfsDriver(remotefs_drv.RemoteFSPoolMixin,
         try:
             if backing_file:
                 temp_file_name = '%s.temp_image.%s.%s' % (
-                    volume['id'],
+                    volume.id,
                     image_meta['id'],
                     root_file_fmt)
                 temp_path = os.path.join(self._local_volume_dir(volume),
@@ -555,7 +555,7 @@ class WindowsSmbfsDriver(remotefs_drv.RemoteFSPoolMixin,
             self.configuration.volume_dd_blocksize)
 
         self._vhdutils.resize_vhd(self.local_path(volume),
-                                  volume['size'] * units.Gi,
+                                  volume.size * units.Gi,
                                   is_file_max_size=False)
 
     def _copy_volume_from_snapshot(self, snapshot, volume, volume_size):
@@ -563,15 +563,15 @@ class WindowsSmbfsDriver(remotefs_drv.RemoteFSPoolMixin,
 
         LOG.debug("snapshot: %(snap)s, volume: %(vol)s, "
                   "volume_size: %(size)s",
-                  {'snap': snapshot['id'],
-                   'vol': volume['id'],
-                   'size': snapshot['volume_size']})
+                  {'snap': snapshot.id,
+                   'vol': volume.id,
+                   'size': snapshot.volume_size})
 
-        info_path = self._local_path_volume_info(snapshot['volume'])
+        info_path = self._local_path_volume_info(snapshot.volume)
         snap_info = self._read_info_file(info_path)
-        vol_dir = self._local_volume_dir(snapshot['volume'])
+        vol_dir = self._local_volume_dir(snapshot.volume)
 
-        forward_file = snap_info[snapshot['id']]
+        forward_file = snap_info[snapshot.id]
         forward_path = os.path.join(vol_dir, forward_file)
 
         # Find the file which backs this file, which represents the point
